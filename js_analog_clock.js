7528ca2bbba77cd6bf4baf9d1ead0f615a6d61c4
John Bennett <jbennett77@gmail.com>
2014-05-02 13:46:25 -0700

    creates minute and hour hands

--- js_analog_clock.js
+++ js_analog_clock.js
@@ -20,10 +20,23 @@ function makeClockFace(){
 }
 
 function makeClockHands(){
+	//create the second hand
 	var secondHand = 7;
 	var secondID = "second";
 	createDivs( secondHand, secondID, "." );
 	moveDivs( secondHand, secondID, 0 );
+
+	//create the minute hand
+	var minuteHand = 7;
+	var minuteID = "minute";
+	createDivs( minuteHand, minuteID, "." );
+	moveDivs( minuteHand, minuteID, 30 ); 
+
+	//create the hour hand
+	var hourHand = 7;
+	var hourID = "hour";
+	createDivs( hourHand, hourID, "." );
+	moveDivs( hourHand, hourID, 60 ); 
 }
  //add divs for the numbers of the clock face to the DOM 
 function createDivs( _numberOfDivs, _idName, _innerHTML ){
@@ -45,33 +58,40 @@ function moveDivs( _numberOfDivs, _idName, _degree ){
 	var degreeOffset = 90;
 	var clockRadius = 50;
 	var incrementValue;
+
+	//checks if creating clock face or hands
+	//if clock face, the for loop will iterate degrees
+	//if clock hands, the for loop will iterate the radius
 	if( _idName === "num" ){
 		incrementValue = degrees / _numberOfDivs;
 	}else{
 		incrementValue = clockRadius / _numberOfDivs;
 		clockRadius = 0;
 	}
-	cout( clockRadius );
-
+	
+	//loop iterates through the set of divs and
+	//either moves the numbers around in a circle
+	//or places the dot divs in a line
 	for( var i = 0; i < _numberOfDivs; i++ ){
 		var top = clockRadius * Math.sin( ( degree - degreeOffset ) * convertToRadian );
 		var left = clockRadius * Math.cos( ( degree - degreeOffset ) * convertToRadian );
 		id( _idName + ( i + 1 ) ).style.position = "absolute";
 		id( _idName + ( i + 1 ) ).style.top = top + "px";
 
-		//fix this for refractoring
+		//if creating the clock face and the numbers are 10 or larger
+		//create an offset to position the numbers a little better
 		if( _idName === "num" && i > 8 ){ 
 			id( _idName + ( i + 1 ) ).style.left = ( left - 4 ) + "px";
 		}else{
 			id( _idName + ( i + 1 ) ).style.left = left + "px";
 		}
-
+		
+		//if creating the clock face,
+		//increment by degrees, otherwise icrement by the radius
 		if( _idName === "num" ){
 			degree += incrementValue;
 		}else{
 			clockRadius += incrementValue;
 		}
-		cout( clockRadius );
-		cout( incrementValue );
 	}
 }
